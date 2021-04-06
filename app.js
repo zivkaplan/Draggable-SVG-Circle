6e3f58fb650e376da1d1a351b5e60eccdfa26415
Ziv Kaplan <zivkaplan@gmail.com>
2021-04-06 21:11:37 +0300

    improved grid user control

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-var dragItem = document.querySelector("#item");
-var container = document.querySelector("#container");
+const dragItem = document.querySelector("#draggable");
+const container = document.querySelector("#container");
 
-// getting the window dimensions  
-const vw = Math.max(document.documentElement.clientWidth)
-const vh = Math.max(document.documentElement.clientHeight)
+const gridCubeSize =100;
 
-// rounding the dimensions
-const vwRounded = roundNum(vw, 100)
-const vhRounded = roundNum(vh, 100)
+// getting the screen dimensions and drawing a full screen grid
+const screenVw = window.screen.width;
+const screenVh = window.screen.height;
 
-//setting the grid port to the rounded dimensions
-container.setAttribute("width", vwRounded)
-container.setAttribute("height", vhRounded)
+// rounding the dimensions to make sure it produces an aligning grid
+const screenVwRounded = roundNum(screenVw, gridCubeSize)
+const screenVhRounded = roundNum(screenVh, gridCubeSize)
 
-//setting the draggable item at the center algined to the 50's multp.
-dragItem.setAttribute('cx', vwRounded / 2)
-dragItem.setAttribute('cy', vhRounded / 2)
-
-//global variables
-var active = false;
-var currentX;
-var currentY;
-var initialX;
-var initialY;
-var xOffset = 0;
-var yOffset = 0;
-
-// func that "moves" the draggabe element
-function setTranslate(xPos, yPos, el) {
-    el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-}
-// function to round number. default is 50.
-function roundNum(num, roundTo = 50) {
-    return Math.round(num / roundTo) * roundTo;
-}
 function gridMaker() {
     // vertical lines
-    for (let i = vwRounded; i >= 0; i -= 50) {
+    for (let i = screenVwRounded; i >= 0; i -= (gridCubeSize / 2)) {
         const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
         line.classList.add('gridLines')
         line.setAttribute('x1', i)
         line.setAttribute('x2', i)
         line.setAttribute('y1', "0")
-        line.setAttribute('y2', vh)
+        line.setAttribute('y2', screenVh)
         container.insertBefore(line, dragItem);
     }
     // horizontal lines
-    for (let i = vhRounded; i >= 0; i -= 50) {
+    for (let i = screenVhRounded; i >= 0; i -= (gridCubeSize / 2)) {
         const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
         line.classList.add('gridLines')
         line.setAttribute('x1', "0")
-        line.setAttribute('x2', vw)
+        line.setAttribute('x2', screenVw)
         line.setAttribute('y1', i)
         line.setAttribute('y2', i)
         container.insertBefore(line, dragItem);
     }
 }
+
+
+// getting the window dimenensions in order to position the draggable item in the middle
+const windowVw = Math.max(document.documentElement.clientWidth)
+const windowVh = Math.max(document.documentElement.clientHeight)
+
+// setting the draggable item at the center of the dimensions that had been rounded to make sure they fit the grid.
+dragItem.setAttribute('cx', roundNum(windowVw, gridCubeSize) / 2)
+dragItem.setAttribute('cy', roundNum(windowVh, gridCubeSize) / 2)
+
+//global variables
+let active = false;
+let currentX;
+let currentY;
+let initialX;
+let initialY;
+let xOffset = 0;
+let yOffset = 0;
+
+// func that "moves" the draggabe element
+function setTranslate(xPos, yPos, el) {
+    el.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
+}
+// function to round number. default is 50.
+function roundNum(num, roundTo) {
+    return Math.round(num / roundTo) * roundTo;
+}
+
 //func for mousedown
 function dragStart(e) {
     if (e.type === "touchstart") {
@@ -65,18 +70,17 @@ function dragStart(e) {
         initialX = e.clientX - xOffset;
         initialY = e.clientY - yOffset;
     }
-
     if (e.target === dragItem) {
         active = true;
     }
 }
 // func for mouseup
 function dragEnd(e) {
-    if (currentX % 50 !== 0) {
-        currentX = roundNum(currentX)
+    if (currentX % (gridCubeSize / 2) !== 0) {
+        currentX = roundNum(currentX, gridCubeSize / 2)
     }
-    if (currentY % 50 !== 0) {
-        currentY = roundNum(currentY)
+    if (currentY % (gridCubeSize / 2) !== 0) {
+        currentY = roundNum(currentY, gridCubeSize / 2)
     }
     setTranslate(currentX, currentY, dragItem);
 
@@ -90,7 +94,9 @@ function dragEnd(e) {
 
 // func for mousemove
 function drag(e) {
-    if (active) {
+
+    if (active){
+    
         e.preventDefault();
 
         if (e.type === "touchmove") {
@@ -109,7 +115,6 @@ function drag(e) {
 }
 
 
-
 // main
 gridMaker();
 container.addEventListener("touchstart", dragStart, false);
@@ -119,11 +124,3 @@ container.addEventListener("touchmove", drag, false);
 container.addEventListener("mousedown", dragStart, false);
 container.addEventListener("mouseup", dragEnd, false);
 container.addEventListener("mousemove", drag, false);
-
-window.addEventListener('resize', () => {
-    newWindowX = Math.max(document.documentElement.clientWidth || 0, window.innerWidth)
-    newWindowY = Math.max(document.documentElement.clientHeight || 0, window.innerHeight)
-    if (newWindowX !== vw || newWindowY !== vh) {
-        location.reload()
-    }
-});
